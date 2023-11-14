7399641c8a8be70c1d92d6c2edf86eb19321504e
amalsaab <441102683@student.ksu.edu.sa>
2023-11-14 10:05:52 +0300

    init solve

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const container = document.getElementById('container')
+const UserName = document.getElementById('UserName')
+const Email = document.getElementById('Email')
+const Password = document.getElementById('Password')
+const form_signup = document.getElementById('form-signup')
 
-fetch("https://fakestoreapi.com/products")
+function products(){
+    fetch("https://fakestoreapi.com/products")
     .then(Response => Response.json())
     .then(data => {console.log(data);
         for (let i = 0; i < data.length; i++) {
@@ -14,3 +19,49 @@ fetch("https://fakestoreapi.com/products")
             
         }
     })
+}
+
+
+    async function signUp(){
+        let userName = UserName.value
+        let email = Email.value
+        let password = Password.value
+        let found = false
+        let text = ""
+        if (userName.length <= 5) {
+            text = "the username is less then 5";
+            found = true
+        } 
+        if (password.length < 8) {
+            text += found? " and the password is less then 8":"the password is less then 8"
+            found = true
+        } 
+        if(found){
+            alert(text)
+            form_signup.action = "index.html"
+        } else{
+            const UserName = document.getElementById('UserName')
+const Email = document.getElementById('Email')
+const Password = document.getElementById('Password')
+try {
+            let response = await fetch("https://655309665449cfda0f2e03c5.mockapi.io/Names", {
+                method: 'POST',
+                body: JSON.stringify({
+                    userName: Password.value,
+                    email:Email.value,
+                    password:password.value
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            });
+            let data = await response.json()
+            console.log(data);
+        }catch(error){
+            console.error(`Download error: ${error.message}`);
+        }
+
+        
+            // form_signup.action = "sginin.html"
+        }
+    }
